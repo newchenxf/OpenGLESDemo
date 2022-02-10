fca8706726c0a273979e2d480be5b6dfa3862163
chenxiaofeng <chenxfemail@gmail.com>
2022-02-10 15:26:02 +0800

    Update ReadMe

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # OpenGLESDemo
 
-基于Android，做一些opengles的例子，包括静态3D模型的渲染，带骨骼动画的3D模型的渲染
+基于Android，做一些OpenGL的例子，包括静态3D模型的渲染，带骨骼动画的3D模型的渲染，文字渲染
 
-数据提取用assimp，渲染用openGL ES.
+3D模型渲染，模型文件的加载，用assimp
+文字渲染，字体文件的加载，使用freetype
 
-
-效果图：
+部分效果图:
 ![Image text](https://raw.githubusercontent.com/newchenxf/OpenGLESDemo/main/pictures/demo.webp)
 
--- app/src/main/cpp/sample/TextSample.cpp
+++ app/src/main/cpp/sample/TextSample.cpp
@@ -10,7 +10,7 @@ static const int MAX_SHORT_VALUE = 65536;
 static const wchar_t CHINESE_COMMON[] = WSTRING_SOURCE(
                                                 ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz12367890的一是了我不人在他有这个上们来到时大地为子中你说生国年着就那和要她出也得里后自以会家可下而过天去能对小多然于心学么之都好看起发当没成只如事把还用第样道想作种开美总从无情己面最女但现前些所同日手又行意动方期它头经长儿回位分爱老因很给名法间斯知世什两次使身者被高已亲其进此话常与活正感见明问力理尔点文几定本公特做外孩相西果走将月十实向声车全信重三机工物气每并别真打太新比才便夫再书部水像眼等体却加电主界门利海受听表德少克代员许稜先口由死安写性马光白或住难望教命花结乐色更拉东神记处让母父应直字场平报友关放至张认接告入笑内英军候民岁往何度山觉路带万男边风解叫任金快原吃妈变通师立象数四失满战远格士音轻目条呢病始达深完今提求清王化空业思切怎非找片罗钱紶吗语元喜曾离飞科言干流欢约各即指合反题必该论交终林请医晚制球决窢传画保读运及则房早院量苦火布品近坐产答星精视五连司巴奇管类未朋且婚台夜青北队久乎越观落尽形影红爸百令周吧识步希亚术留市半热送兴造谈容极随演收首根讲整式取照办强石古华諣拿计您装似足双妻尼转诉米称丽客南领节衣站黑刻统断福城故历惊脸选包紧争另建维绝树系伤示愿持千史谁准联妇纪基买志静阿诗独复痛消社算算义竟确酒需单治卡幸兰念举仅钟怕共毛句息功官待究跟穿室易游程号居考突皮哪费倒价图具刚脑永歌响商礼细专黄块脚味灵改据般破引食仍存众注笔甚某沉血备习校默务土微娘须试怀料调广蜖苏显赛查密议底列富梦错座参八除跑亮假印设线温虽掉京初养香停际致阳纸李纳验助激够严证帝饭忘趣支春集丈木研班普导顿睡展跳获艺六波察群皇段急庭创区奥器谢弟店否害草排背止组州朝封睛板角况曲馆育忙质河续哥呼若推境遇雨标姐充围案伦护冷警贝著雪索剧啊船险烟依斗值帮汉慢佛肯闻唱沙局伯族低玩资屋击速顾泪洲团圣旁堂兵七露园牛哭旅街劳型烈姑陈莫鱼异抱宝权鲁简态级票怪寻杀律胜份汽右洋范床舞秘午登楼贵吸责例追较职属渐左录丝牙党继托赶章智冲叶胡吉卖坚喝肉遗救修松临藏担戏善卫药悲敢靠伊村戴词森耳差短祖云规窗散迷油旧适乡架恩投弹铁博雷府压超负勒杂醒洗采毫嘴毕九冰既状乱景席珍童顶派素脱农疑练野按犯拍征坏骨余承置臓彩灯巨琴免环姆暗换技翻束增忍餐洛塞缺忆判欧层付阵玛批岛项狗休懂武革良恶恋委拥娜妙探呀营退摇弄桌熟诺宣银势奖宫忽套康供优课鸟喊降夏困刘罪亡鞋健模败伴守挥鲜财孤枪禁恐伙杰迹妹藸遍盖副坦牌江顺秋萨菜划授归浪听凡预奶雄升碃编典袋莱含盛济蒙棋端腿招释介烧误);
 static const wchar_t CHINESE_TEST[] = WSTRING_SOURCE(Love小爱心HAHA);
-
+#define ENABLE_ROTATE 0
 
 const char vShaderStr[] =
         "#version 300 es                       \n"
@@ -199,19 +199,14 @@ void TextSample::Draw(int screenW, int screenH) {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     m_pShader->use();
 
-    //glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(screenW), 0.0f,
-    //                                 static_cast<float>(screenH));
-
     glm::vec2 viewport(screenW, screenH);
     UpdateMVPMatrix(m_MVPMatrix, m_AngleX, m_AngleY, viewport.x / viewport.y);
 
     m_pShader->setMat4("u_MVPMatrix", m_MVPMatrix);
 
     RenderTextChinese(m_pShader, CHINESE_TEST,
-                      sizeof(CHINESE_TEST) / sizeof(CHINESE_TEST[0]) - 1, -0.9f, -0.1f, 1.0f,
+                      sizeof(CHINESE_TEST) / sizeof(CHINESE_TEST[0]) - 1, -0.5f, 0.0f, 1.0f,
                       glm::vec3(0.5f, 0.8f, 0.2f), viewport);
-
-
 }
 
 void TextSample::UpdateTransformMatrix(float rotateX, float rotateY, float scaleX, float scaleY) {
@@ -240,8 +235,6 @@ void TextSample::UpdateMVPMatrix(glm::mat4 &mvpMatrix, int angleX, int angleY, f
 
     // Projection matrix
     glm::mat4 Projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.1f, 100.0f);
-    //glm::mat4 Projection = glm::frustum(-ratio, ratio, -1.0f, 1.0f, 4.0f, 100.0f);
-    //glm::mat4 Projection = glm::perspective(45.0f,ratio, 0.1f,100.f);
 
     // View matrix
     glm::mat4 View = glm::lookAt(
@@ -253,8 +246,10 @@ void TextSample::UpdateMVPMatrix(glm::mat4 &mvpMatrix, int angleX, int angleY, f
     // Model matrix
     glm::mat4 Model = glm::mat4(1.0f);
     Model = glm::scale(Model, glm::vec3(m_ScaleX, m_ScaleY, 1.0f));
+#if ENABLE_ROTATE
     Model = glm::rotate(Model, radiansX, glm::vec3(1.0f, 0.0f, 0.0f));
     Model = glm::rotate(Model, radiansY, glm::vec3(0.0f, 1.0f, 0.0f));
+#endif
     Model = glm::translate(Model, glm::vec3(0.0f, 0.0f, 0.0f));
 
     mvpMatrix = Projection * View * Model;
